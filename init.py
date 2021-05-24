c08c268d6b6365007b73bfd680a28e4b3a0aee50
17553047761 <64392344+17553047761@users.noreply.github.com>
2021-05-24 20:30:07 +0800

    Update init.py

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 TOKEN = '1836395637:AAEFLN82cMMOMAGgwFhfFTpUXbZqXvF7F04'
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
