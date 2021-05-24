8df63f3e2bba43bfa11b9a682865b8448930bde8
17553047761 <64392344+17553047761@users.noreply.github.com>
2021-05-24 20:31:54 +0800

    Update init.py

--- init.py
+++ init.py
@@ -8,5 +8,5 @@ updater = Updater(TOKEN)
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN,
-                      webhook_url="https://<appname>.herokuapp.com/" + TOKEN)
+                      webhook_url="https://awdawda123.herokuapp.com/" + TOKEN)
 updater.idle()
