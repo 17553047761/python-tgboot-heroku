ff187f14608cdb401f0e297f816e090555b86a00
17553047761 <64392344+17553047761@users.noreply.github.com>
2021-05-24 20:17:54 +0800

    Update init.py

--- init.py
+++ init.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOURTELEGRAMBOTTOKEN'
+TOKEN = '1836395637:AAEFLN82cMMOMAGgwFhfFTpUXbZqXvF7F04'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -34,7 +34,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater('1836395637:AAEFLN82cMMOMAGgwFhfFTpUXbZqXvF7F04', use_context=True)
+    updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
